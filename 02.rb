4c375bdec80c4df975c9cf574b5ff83f85d82d08
Aline Freitas <aline.do.freitas@gmail.com>
2018-08-29 14:32:43 +0000

    Added description of first 10 problems

--- 02.rb
+++ 02.rb
@@ -1,5 +1,6 @@
-# By considering the terms in the Fibonacci sequence whose values
-# do not exceed four million, find the sum of the even-valued terms.
+# By considering the terms in the Fibonacci sequence
+# whose values do not exceed four million,
+# find the sum of the even-valued terms.
 
 fib1 = 0
 fib2 = 1
